984ca146d055535d3edc544f3264acb7dd7f45da
twinkalnutech-eng <twinkalnutech@gmail.com>
2026-01-13 15:20:52 +0530

    Update database.py
    os data add

--- core/database.py
+++ core/database.py
@@ -1,13 +1,25 @@
-import pyodbc
+# core/database.py
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+ENV = os.getenv("APP_ENV", "local").lower()
 
 def get_connection():
-    return pyodbc.connect(
-        f"SERVER={os.getenv('DB_SERVER')};"
-        f"DATABASE={os.getenv('DB_DATABASE')};"
-        f"UID={os.getenv('DB_USERNAME')};"
-        f"PWD={os.getenv('DB_PASSWORD')}"
-    )
+    if ENV == "production":
+        import pymssql
+        return pymssql.connect(
+            server=os.getenv("DB_SERVER"),
+            user=os.getenv("DB_USERNAME"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_DATABASE"),
+            port=int(os.getenv("DB_PORT", 1433))
+        )
+    else:
+        import pyodbc
+        return pyodbc.connect(
+            f"DRIVER={{{os.getenv('DB_DRIVER')}}};"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_DATABASE')};"
+            f"UID={os.getenv('DB_USERNAME')};"
+            f"PWD={os.getenv('DB_PASSWORD')};"
+            "TrustServerCertificate=yes;"
+        )
