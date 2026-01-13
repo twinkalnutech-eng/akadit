79e6063884aa3378adf3e982456b1fe4160baefd
twinkalnutech-eng <twinkalnutech@gmail.com>
2026-01-13 14:27:48 +0530

    add database connections

--- core/database.py
+++ core/database.py
@@ -1,18 +1,15 @@
-import pymssql
+import pyodbc
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
-    server = os.getenv("DB_SERVER")
-    database = os.getenv("DB_DATABASE")
-    user = os.getenv("DB_USERNAME")
-    password = os.getenv("DB_PASSWORD")
-    port = int(os.getenv("DB_PORT", 1433))
-
-    conn = pymssql.connect(
-        server=server,
-        user=user,
-        password=password,
-        database=database,
-        port=port
+    return pyodbc.connect(
+        f"DRIVER={{{os.getenv('DB_DRIVER')}}};"
+        f"SERVER={os.getenv('DB_SERVER')};"
+        f"DATABASE={os.getenv('DB_DATABASE')};"
+        f"UID={os.getenv('DB_USERNAME')};"
+        f"PWD={os.getenv('DB_PASSWORD')}"
     )
-    return conn
+
